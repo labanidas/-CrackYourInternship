be0f0961d407a503700d997894a4aae03b83c03e
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-08-07 11:08:35 -0700

    Create 60. Permutations in array.cpp

---
+++ 60. Permutations in array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        
+        int n = arr1.size();
+        
+        sort(arr1.begin(), arr1.end());
+        
+        sort(arr2.begin(), arr2.end(), greater<int>());
+        
+        for(int i=0; i<n; i++){
+            if(arr1[i]+arr2[i] < k) return false;
+        }
+        return true;
+    }
+};
