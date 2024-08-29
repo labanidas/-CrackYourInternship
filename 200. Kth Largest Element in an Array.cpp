1a9a85b33c4473e41414744f1d8c8d8163c5ede1
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-08-29 11:00:23 -0700

    Create 200. Kth Largest Element in an Array.cpp

---
+++ 200. Kth Largest Element in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq; // max heap
+
+        for (int num : nums) {
+            pq.push(num);
+        }
+
+        while (k-- >1) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
