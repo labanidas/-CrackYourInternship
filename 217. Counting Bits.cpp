61c7a6cd1364aac6b45f600f6aefb4ec39fe6847
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-09-01 10:08:17 -0700

    Create 217. Counting Bits.cpp

---
+++ 217. Counting Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // question --  return array of number of set bits 
+    vector<int> countBits(int n) {
+        
+        vector<int> ans;
+
+        for(int i=0 ; i<=n; i++){
+            ans.push_back( __builtin_popcount(i));
+        }
+
+        return ans;
+    }
+};
