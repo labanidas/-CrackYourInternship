94a689ed542532dc79a22b1bd2ccbf85e0072250
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-09-11 19:06:05 -0700

    Create 283. Optimal Binary Search Tree.cpp

---
+++ 283. Optimal Binary Search Tree.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int freq[], int i, int j){
+    int s = 0;
+    for(int k = i; k<=j; k++){
+        s += freq[k];
+    }
+    
+    return s;
+}
+
+int optCost(int freq[], int i, int j){
+    
+    if(j < i) return 0;
+    if(j == i) return freq[i];
+    
+    int fsum = sum(freq, i, j);
+    
+    int min_cost = INT_MAX;
+    
+    for(int r = i; r <= j; r++){
+        int cost = optCost(freq, i, r-1) + optCost(freq, r+1, j);
+        min_cost = min(min_cost, cost);
+    }
+    
+    return min_cost + fsum;
+}
+
+int optimalSearchTree(int keys[], int freq[], int n){
+    return optCost(freq, 0, n-1);
+}
+
+
+// Driver Code
+int main() 
+{ 
+    int keys[] = {10, 12, 20}; 
+    int freq[] = {34, 8, 50}; 
+    int n = sizeof(keys) / sizeof(keys[0]); 
+    cout << "Cost of Optimal BST is "
+         << optimalSearchTree(keys, freq, n); 
+    return 0; 
+} 
+ 
